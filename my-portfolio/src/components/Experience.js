9c5ec0c360c25e6e9b54311737c423b1c16df249
grishma <gcpatel888@gmail.com>
2024-09-19 15:23:20 -0400

    init

--- my-portfolio/src/components/Experience.js
+++ my-portfolio/src/components/Experience.js
@@ -5,11 +5,11 @@ import React from 'react';
 const Experience = ({ experiences  }) => {
   return (
     <div className="experience">
-     <h2>Work Experience</h2>
+     <h2>Projects</h2>
       {experiences.map((experience, index) => (
         <div key={index} className="experience">
-          <h3>{experience.title} at {experience.company}</h3>
-          <p>{experience.startDate} - {experience.endDate}</p>
+          <h3>{experience.title}</h3>
+          {/* <p>{experience.startDate} - {experience.endDate}</p> */}
          
           <ui>{experience.description.map((item, idx)=>(
             <li key={idx}>
--- my-portfolio/src/components/Portfolio.js
+++ my-portfolio/src/components/Portfolio.js
@@ -10,7 +10,7 @@ const Portfolio = () => {
   const profileData = {
     name: 'Grishma Patel',
     title: 'Full-Stack Developer',
-    description: 'Passionate developer with a knack for creating dynamic web applications.Experience working alongside other developers and testers to build high-quality web products.',
+    description: 'Hi, I am a Passionate software developer with a knack for creating dynamic and scalable web applications.Experience working alongside other developers and testers to build high-quality web products.I enjoy solving complex problmes.',
     // image: 'logo192.jpg', // Make sure to place an image in the public folder or use a URL
   };
 
@@ -18,43 +18,43 @@ const Portfolio = () => {
   
   const experienceData = [
     {
-        title: 'Full-Stack Developer',
+        title: 'Video Sync',
         company: 'Lsquared Digital Signage',
         startDate: 'May 2023',
         endDate: 'June 2024',
         description: [
-        'Leading the development of web applications using React and Node.js.',
-        'Collaborating with cross-functional teams to define, design, and ship new features.',
-        'Mentoring junior developers and conducting code reviews to ensure high code quality.'
+        'Leading the development of video sync feature to enable video play at same time on multiple Tv screens.',
+        'Used Websocket brodcasting for signaling video to play at a scheduled time.',
+        'Collaborating with cross-functional teams to define, design, and ship new feature.'
         ],
     },
       {
-        title: 'Software Engineer',
+        title: 'Video Wall',
         company: 'CPP Investment',
         startDate: 'Jan 2021',
         endDate: 'April 2023',
         description: [
-            'Developed and maintained web applications using JavaScript and Python.',
-            'Participated in the full software development lifecycle from planning to deployment.',
-            'Worked closely with clients to gather requirements and provide technical solutions.'
+          'Video wall feature to enable video crop in different n:n or n:m sized and display in Tv screens placed together as video wall',
+            'Developed and maintained digital signage applications using JavaScript and Python.',
+            'Participated in the full software development lifecycle from planning to deployment.'
         ],
       },
       {
-        title: 'Python Developer',
+        title: 'API Test',
         company: 'Accenture',
         startDate: 'June 2018',
         endDate: 'May 2019',
         description: [
-            'Developed and maintained web applications using JavaScript and Python.',
-            'Participated in the full software development lifecycle from planning to deployment.',
-            'Worked closely with Senior Developer to gather requirements and understand technical solutions.'
+            'Developed Pytest cases in Python for testing REST api end-points.',
+            'With ongoing deployments making sure the reliability of API and getting repots on test results.',
+            'Automated testing with Docker containers for getting real time reporting.'
         ],
       },
   ];
 
   const contactData = {
     email: 'grishmapatel.0821@gmail.com',
-    linkedIn: 'https://www.linkedin.com/gpatel',
+    linkedIn: 'https://www.linkedin.com/grishma-patel-gp17',
     github: 'https://github.com/Grishmacoder',
   };
 
